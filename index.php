733bbe1159d0833680b464ca0aad7e9c9340e40f
glyn.davies <glynwdavies@gmail.com>
2018-03-26 17:12:54 +0200

    Added HTML markup to index.php

--- index.php
+++ index.php
@@ -1,1 +1,52 @@
 <?php include 'core/init.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en-GB">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Chirper</title>
+    <link rel="stylesheet" href="assets/css/font/css/font-awesome.css">
+    <link rel="stylesheet" href="assets/css/style-complete.css">
+</head>
+<body>
+    <div class="front-img">
+        <img src="assets/images/background.jpg" alt="Backgroud image">
+    </div>
+    <div class="wrapper">
+        <div class="header-wrapper">
+            <div class="nav-container">
+                <div class="nav">
+                    <div class="nav-left">
+                        <ul>
+                            <li><i class="fa fa-twitter" aria-hidden="true"></i><a href="#"></a></li>
+                            <li><a href="#">About</a></li>
+                        </ul>
+                    </div>
+                    <div class="nav-right">
+                        <ul>
+                            <li><a href="#">Language</a></li>
+                        </ul>
+                    </div><!-- end of nav-right -->
+                </div><!-- end of nav -->
+            </div><!-- end of nav-container -->
+        </div><!-- end of header-wrapper -->
+        <div class="inner-wrapper">
+            <div class="main-container">
+                <div class="content-left">
+                    <h1>Welcome to Chirper</h1>
+                    <br>
+                    <p>A place to connect with the rest of the world!</p>
+                </div><!-- end of content-left -->
+                <div class="content-right">
+                    <div class="login-wrapper">
+                    </div><!-- end of login-wrapper -->
+                    <div class="signup-wrapper">
+                    </div><!-- end of signup-wrapper -->
+                </div><!-- end of content-right -->
+            </div><!-- end of main-container -->
+        </div><!-- end of inner-wrapper -->
+    </div><!-- end of wrapper -->
+</body>
+</html>
